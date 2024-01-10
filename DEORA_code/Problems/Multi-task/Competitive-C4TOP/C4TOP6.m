1b9f7d86ede75b10900843e66524a29f3261be7b
Lindong Xie <138126615+ForrestXie9@users.noreply.github.com>
2024-01-10 21:15:13 +0800

    Add files via upload

---
+++ DEORA_code/Problems/Multi-task/Competitive-C4TOP/C4TOP6.m
@@ -0,0 +1,28 @@
+classdef C4TOP6 < Problem
+    % <Multi-task> <Single-objective> <Competitive>
+
+    %------------------------------- Copyright --------------------------------
+    % Copyright (c) 2022 Yanchi Li. You are free to use the MTO-Platform for
+    % research purposes. All publications which use this platform or any code
+    % in the platform should acknowledge the use of "MTO-Platform" and cite
+    % or footnote "https://github.com/intLyc/MTO-Platform"
+    %--------------------------------------------------------------------------
+
+    methods
+        function Prob = C4TOP6(varargin)
+            Prob = Prob@Problem(varargin);
+            Prob.maxFE = 500 * 100 * 4;
+        end
+
+        function setTasks(Prob)
+            Tasks = benchmark_C4TOP(6);
+            Prob.T = length(Tasks);
+            for t = 1:Prob.T
+                Prob.D(t) = Tasks(t).Dim;
+                Prob.Fnc{t} = Tasks(t).Fnc;
+                Prob.Lb{t} = Tasks(t).Lb;
+                Prob.Ub{t} = Tasks(t).Ub;
+            end
+        end
+    end
+end
