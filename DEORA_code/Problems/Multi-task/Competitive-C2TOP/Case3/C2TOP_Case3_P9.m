aba7a2a0d4180cd378351573636e76e1edab2eff
Lindong Xie <138126615+ForrestXie9@users.noreply.github.com>
2024-01-10 21:16:30 +0800

    Add files via upload

---
+++ DEORA_code/Problems/Multi-task/Competitive-C2TOP/Case3/C2TOP_Case3_P9.m
@@ -0,0 +1,21 @@
+classdef C2TOP_Case3_P9 < Problem
+    % <Multi-task> <Single-objective> <Competitive>
+
+    methods
+        function Prob = C2TOP_Case3_P9(varargin)
+            Prob = Prob@Problem(varargin);
+            Prob.maxFE = 1000 * 100 * 2;
+        end
+
+        function setTasks(Prob)
+            Tasks = benchmark_CEC17_MTSO_Competitive(9, 3);
+            Prob.T = length(Tasks);
+            for t = 1:Prob.T
+                Prob.D(t) = Tasks(t).Dim;
+                Prob.Fnc{t} = Tasks(t).Fnc;
+                Prob.Lb{t} = Tasks(t).Lb;
+                Prob.Ub{t} = Tasks(t).Ub;
+            end
+        end
+    end
+end
---
+++ DEORA_code/Problems/Multi-task/Competitive-C2TOP/Case4/C2TOP_Case4_P5.m
@@ -0,0 +1,21 @@
+classdef C2TOP_Case4_P5 < Problem
+    % <Multi-task> <Single-objective> <Competitive>
+
+    methods
+        function Prob = C2TOP_Case4_P5(varargin)
+            Prob = Prob@Problem(varargin);
+            Prob.maxFE = 1000 * 100 * 2;
+        end
+
+        function setTasks(Prob)
+            Tasks = benchmark_CEC17_MTSO_Competitive(5, 4);
+            Prob.T = length(Tasks);
+            for t = 1:Prob.T
+                Prob.D(t) = Tasks(t).Dim;
+                Prob.Fnc{t} = Tasks(t).Fnc;
+                Prob.Lb{t} = Tasks(t).Lb;
+                Prob.Ub{t} = Tasks(t).Ub;
+            end
+        end
+    end
+end
